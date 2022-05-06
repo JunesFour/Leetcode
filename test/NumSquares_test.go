2a39ba0bed27f41aae33393580af0020a260f82c
dawnlighttt <501124524@qq.com>
2022-05-06 20:02:52 +0800

    最近更新： 搜索二维矩阵2 完全平方数

---
+++ test/NumSquares_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+)
+
+func numSquares(n int) int {
+	return 1
+
+}
+
+func TestNumSquares(t *testing.T) {
+
+}
---
+++ 回溯/搜索二维矩阵2/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func searchMatrix(matrix [][]int, target int) bool {
+	var find bool
+	visited := make([][]bool, len(matrix))
+	for i := 0; i < len(matrix); i++ {
+		visited[i] = make([]bool, len(matrix[i]))
+	}
+	var dfs func(i, j int)
+	dfs = func(i, j int) {
+		visited[i][j] = true
+		// 找到数字
+		if matrix[i][j] == target {
+			find = true
+			return
+		}
+		if find == true {
+			return
+		}
+		//fmt.Println("i: ", i, ", j: ", j)
+		// 未出下边界
+		if i+1 < len(matrix) && matrix[i+1][j] <= target && !visited[i+1][j] {
+			dfs(i+1, j)
+		}
+		// 未出下边界
+		if j+1 < len(matrix[0]) && matrix[i][j+1] <= target && !visited[i][j+1] {
+			dfs(i, j+1)
+		}
+	}
+	dfs(0, 0)
+	return find
+}
+
+func searchMatrix1(matrix [][]int, target int) bool {
+	for _, row := range matrix {
+		i := sort.SearchInts(row, target)
+		if i < len(row) && row[i] == target {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(searchMatrix([][]int{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 20))
+}
