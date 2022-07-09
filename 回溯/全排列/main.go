47c67d42749fcd1e686e9df190d4835ce8e50ff2
dawnlighttt <501124524@qq.com>
2022-07-09 12:31:56 +0800

    最近更新：全排列添加解法

--- 回溯/全排列/main.go
+++ 回溯/全排列/main.go
@@ -28,6 +28,32 @@ func dfs(nums []int, cur []int, res *[][]int) {
 	}
 }
 
+// 使用标记数组
+func permute1(nums []int) (res [][]int) {
+	var cur []int
+	visited := make([]bool, len(nums))
+	var dfs func(index int)
+	dfs = func(index int) {
+		if len(cur) == len(nums) {
+			res = append(res, append([]int{}, cur...))
+			return
+		}
+
+		for i := 0; i < len(nums); i++ {
+			if visited[i] {
+				continue
+			}
+			visited[i] = true
+			cur = append(cur, nums[i])
+			dfs(i)
+			visited[i] = false
+			cur = cur[:len(cur)-1]
+		}
+	}
+	dfs(0)
+	return res
+}
+
 func main() {
 
 }
